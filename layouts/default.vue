25a7472e4003f6e79952d448bebc21e910feede9
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-15 15:09:03 +0300

    Added monserrat font

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,11 @@
 </template>
 
 <style>
-
+@font-face {
+  font-family: "Montserrat";
+  src: local("~assets/fonts/Montserrat-Regular.ttf");
+}
+body {
+  font-family: "Montserrat", sans-serif;
+}
 </style>
