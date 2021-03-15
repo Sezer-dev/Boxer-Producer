f05dd5f32ab57f139ac7a270fee66a61d171d07c
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-15 15:09:57 +0300

    Added Hero component

---
+++ components/Hero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row d-flex align-items-center">
+      <div class="col-md-8">
+        <p class="hero-text w-100">
+          We design the most comfortable men’s
+          <span class="text-alert">underwear.</span>
+        </p>
+      </div>
+      <div class="col-md-4">
+        <img
+          class="hero-image"
+          src="~/assets/images/full-body.jpg"
+          alt="Boxer"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.hero-image {
+  width: 466px;
+  height: 463px;
+}
+.hero-text {
+  font-size: 64px;
+  letter-spacing: 8px;
+  line-height: 82px;
+}
+.text-alert {
+  color: #d81a04;
+}
+</style>
