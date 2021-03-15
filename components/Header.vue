556bab1989e3ca2dc8d8cce51b661f7e02223228
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-15 15:10:44 +0300

    Added Header component

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container-fluid border-bottom border-dark">
+    <div
+      class="container d-flex justify-content-between align-items-center py-4"
+    >
+      <Logo />
+      <button class="contact-button">CONTACT US</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.contact-button {
+  background-color: black;
+  border-radius: 25px;
+  height: 50px;
+  width: 200px;
+  color: white;
+}
+</style>
