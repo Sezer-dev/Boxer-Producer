a97f980f536787bbca4ef134be7a55f3741c200a
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-15 15:12:21 +0300

    Added Header, Navbar, Hero, About components to the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-  
+  <div class="w-100">
+    <Header />
+    <Navbar />
+    <Hero />
+    <About />
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
