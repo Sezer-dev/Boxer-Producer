8036e8078bf1460d4e4795cf9136a45e2c2613e8
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-15 15:09:40 +0300

    Added Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-nav class="container mt-3 mb-5">
+    <b-nav-item to="/">HOME</b-nav-item>
+    <b-nav-item to="#about">ABOUT</b-nav-item>
+    <b-nav-item to="#products">WHAT WE PRODUCE</b-nav-item>
+    <b-nav-item to="contact">CONTACT</b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+ul li a {
+  color: black !important;
+  font-size: 18px;
+}
+</style>
