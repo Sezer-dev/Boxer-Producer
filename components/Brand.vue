da5b839ab7bdaf293f5d2358a2d8f31af1a7e5ab
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-17 16:03:00 +0300

    Added Brand component

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-3 offset-1">
-        <img src="~/assets/images/boxer-front.jpg" alt="White Boxer" />
+  <div class="container-fluid my-5 py-5">
+    <div
+      class="row col-md-10 offset-1 d-flex justify-content-between align-items-center"
+    >
+      <div class="col-md-3">
+        <img
+          class="boxer-front"
+          src="~/assets/images/boxer-front.jpg"
+          alt="White Boxer"
+        />
       </div>
-      <div></div>
+      <div class="col-md-6">
+        <div class="brand-title">
+          <p>
+            The Biggest Boxershort Manufacturer in
+            <span class="text-alert">Turkey.</span>
+          </p>
+        </div>
+        <div class="brand-text">
+          <p>
+            We are one of the companies that exports the high volume boxershorts
+            from Turkey.
+          </p>
+          <p>
+            Custom Design, Custom ideas; open for every entrepreneur
+            individual/companies with reasonable Minimum Order Quantity.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,4 +36,18 @@
 export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.boxer-front {
+  width: 343px;
+  height: 437px;
+}
+.text-alert {
+  color: #d81a04;
+}
+.brand-title p {
+  font-size: 48px;
+}
+.brand-text p {
+  font-size: 24px;
+}
+</style>
