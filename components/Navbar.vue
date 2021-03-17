053c95f954e9b3a4cde1a80dd946fe5b23e648c1
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-17 16:31:20 +0300

    Added border to the bottom of the links when the route matches exactly

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="container mt-3 mb-5 navbar-container">
     <b-nav>
-      <b-nav-item to="/">HOME</b-nav-item>
-      <b-nav-item to="#about">ABOUT</b-nav-item>
-      <b-nav-item to="#products">WHAT WE PRODUCE</b-nav-item>
-      <b-nav-item to="#contact">CONTACT</b-nav-item>
+      <b-nav-item exact-active-class="active" class="nav-link" to="/"
+        >HOME</b-nav-item
+      >
+      <b-nav-item exact-active-class="active" class="nav-link" to="#about"
+        >ABOUT</b-nav-item
+      >
+      <b-nav-item exact-active-class="active" class="nav-link" to="#products"
+        >WHAT WE PRODUCE</b-nav-item
+      >
+      <b-nav-item exact-active-class="active" class="nav-link" to="#contact"
+        >CONTACT</b-nav-item
+      >
     </b-nav>
   </div>
 </template>
@@ -20,5 +28,12 @@ ul li a {
 }
 .navbar-container {
   background-color: #ffffff;
+}
+.nav-link a {
+  padding: 0;
+  margin: 8px 16px;
+}
+.active {
+  border-bottom: 2px solid #d81a04;
 }
 </style>
