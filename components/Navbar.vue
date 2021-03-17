a52aad5dd75632bcf4433f5443b02af3deccf982
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-17 16:05:36 +0300

    Added container div for the maintainability

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,12 @@
 <template>
-  <b-nav class="container mt-3 mb-5">
-    <b-nav-item to="/">HOME</b-nav-item>
-    <b-nav-item to="#about">ABOUT</b-nav-item>
-    <b-nav-item to="#products">WHAT WE PRODUCE</b-nav-item>
-    <b-nav-item to="contact">CONTACT</b-nav-item>
-  </b-nav>
+  <div class="container mt-3 mb-5 navbar-container">
+    <b-nav>
+      <b-nav-item to="/">HOME</b-nav-item>
+      <b-nav-item to="#about">ABOUT</b-nav-item>
+      <b-nav-item to="#products">WHAT WE PRODUCE</b-nav-item>
+      <b-nav-item to="#contact">CONTACT</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
@@ -15,5 +17,8 @@ export default {}
 ul li a {
   color: black !important;
   font-size: 18px;
+}
+.navbar-container {
+  background-color: #ffffff;
 }
 </style>
