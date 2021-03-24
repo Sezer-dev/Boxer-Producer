8aa2fc31906325caa947fa5b6ff698d1a39fe0c2
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-24 19:03:46 +0300

    Responsive styling

--- components/Brand.vue
+++ components/Brand.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="container-fluid my-5 py-5">
+  <div class="container-fluid my-5">
     <div
-      class="row col-md-10 offset-1 d-flex justify-content-between align-items-center"
+      class="row d-flex flex-column flex-md-row justify-content-md-center align-items-center brand-row"
     >
-      <div class="col-md-3">
-        <img
-          class="boxer-front"
-          src="~/assets/images/boxer-front.jpg"
-          alt="White Boxer"
-        />
-      </div>
-      <div class="col-md-6">
-        <div class="brand-title">
-          <p>
-            The Biggest Boxershort Manufacturer in
-            <span class="text-alert">Turkey.</span>
-          </p>
+      <div
+        class="col-md-10 col-12 d-flex flex-md-row flex-column justify-content-md-between justify-content-center align-items-center"
+      >
+        <div class="col-md-5 col-12 d-flex justify-content-center">
+          <img
+            class="boxer-front"
+            src="~/assets/images/boxer-front.jpg"
+            alt="White Boxer"
+          />
         </div>
-        <div class="brand-text">
-          <p>
-            We are one of the companies that exports the high volume boxershorts
-            from Turkey.
-          </p>
-          <p>
-            Custom Design, Custom ideas; open for every entrepreneur
-            individual/companies with reasonable Minimum Order Quantity.
-          </p>
+        <div class="col-md-7 col-12">
+          <div class="brand-title">
+            <p class="text-center text-md-left">
+              The Biggest Boxershort Manufacturer in
+              <span class="text-alert">Turkey.</span>
+            </p>
+          </div>
+          <div class="brand-text">
+            <p>
+              We are one of the companies that exports the high volume
+              boxershorts from Turkey.
+            </p>
+            <p>
+              Custom Design, Custom ideas; open for every entrepreneur
+              individual/companies with reasonable Minimum Order Quantity.
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -51,5 +55,20 @@ export default {
 }
 .brand-text p {
   font-size: 24px;
+}
+@media screen and (max-width: 768px) {
+  .brand-title p {
+    font-size: 24px;
+  }
+  .brand-text p {
+    font-size: 12px;
+  }
+  .boxer-front {
+    width: 186px;
+    height: 237px;
+  }
+  .brand-row {
+    margin: 0;
+  }
 }
 </style>
