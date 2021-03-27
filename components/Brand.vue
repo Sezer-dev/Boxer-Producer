e7b9faf483beb9ca7af184d3880852a31873ba64
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-27 22:43:59 +0300

    Changed row columns to 11

--- components/Brand.vue
+++ components/Brand.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid my-5">
     <div class="row d-flex justify-content-center align-items-center brand-row">
       <div
-        class="col-12 d-flex flex-md-row flex-column justify-content-md-between justify-content-center align-items-center"
+        class="col-11 d-flex flex-md-row flex-column justify-content-md-between justify-content-center align-items-center"
       >
         <div
           class="col-md-5 col-12 d-flex justify-content-md-start justify-content-center"
