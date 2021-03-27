eb2d24fb02cb92c16e636ea468d0c63360dc7e66
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-27 22:24:57 +0300

    Added collapse and responsive styling

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="container mt-3 mb-5 navbar-container">
-    <b-nav>
-      <b-nav-item exact-active-class="active" class="nav-link" to="/"
-        >HOME</b-nav-item
-      >
-      <b-nav-item exact-active-class="active" class="nav-link" to="#about"
-        >ABOUT</b-nav-item
-      >
-      <b-nav-item exact-active-class="active" class="nav-link" to="#products"
-        >WHAT WE PRODUCE</b-nav-item
-      >
-      <b-nav-item exact-active-class="active" class="nav-link" to="#contact"
-        >CONTACT</b-nav-item
-      >
-    </b-nav>
-  </div>
+  <b-navbar
+    toggleable="md"
+    class="container d-flex mt-1 mb-5 pl-0 navbar-container"
+  >
+    <b-collapse id="toggle-nav" is-nav>
+      <b-navbar-nav fill>
+        <b-nav-item
+          exact-active-class="active"
+          class="nav-link py-0 py-md-2"
+          to="/"
+        >
+          HOME
+        </b-nav-item>
+        <b-nav-item exact-active-class="active" class="nav-link" to="#about">
+          ABOUT
+        </b-nav-item>
+        <b-nav-item exact-active-class="active" class="nav-link" to="#products">
+          WHAT WE PRODUCE
+        </b-nav-item>
+        <b-nav-item exact-active-class="active" class="nav-link" to="#contact">
+          CONTACT
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -24,6 +33,9 @@ export default {
 </script>
 
 <style scoped>
+ul li {
+  cursor: pointer;
+}
 ul li a {
   color: black !important;
   font-size: 18px;
