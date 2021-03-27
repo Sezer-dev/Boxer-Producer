ec60419ec9784842a0963787ae9be89f03f72f07
Sezer-dev <Sezer-dev@users.noreply.github.com>
2021-03-27 22:45:36 +0300

    Changed row column to 11

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="container-fluid bg-white py-md-5 py-0 mt-md-5 mt-0">
     <div class="row d-flex justify-content-center footer-row">
       <div
-        class="col-12 d-flex flex-md-row flex-column justify-content-center justify-content-md-between align-items-center"
+        class="col-11 d-flex flex-md-row flex-column justify-content-center justify-content-md-between align-items-center"
       >
         <div class="py-2 py-md-0">
           <Logo />
